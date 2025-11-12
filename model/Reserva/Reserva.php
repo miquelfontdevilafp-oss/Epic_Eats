0155ea6a529856cf691c1be359d982f26e9d7527
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2025-11-12 19:55:01 +0100

    Creacio classe reserva i constructors de clase rezerva i usuari 12/11/25

---
+++ model/Reserva/Reserva.php
@@ -0,0 +1,107 @@
+<?php
+
+class Reserva{
+    private int $id;
+    private DateTime $data;
+    private DateTime $hora;
+    private int $numeroPersones;
+    private int $id_usuari;
+
+    public function __construct(int $id, DateTime $data, DateTime $hora, int $numeroPersones, int $id_usuari){
+            $this->id = $id;
+            $this->data = $data;
+            $this->hora = $hora;
+            $this->numeroPersones = $numeroPersones;
+            $this->id_usuari = $id_usuari;
+        }
+
+    /**
+     * Get the value of id_usuari
+     */ 
+    public function getId_usuari(){
+        return $this->id_usuari;
+    }
+
+    /**
+     * Get the value of numeroPersones
+     */ 
+    public function getNumeroPersones(){
+        return $this->numeroPersones;
+    }
+
+    /**
+     * Get the value of hora
+     */ 
+    public function getHora(){
+        return $this->hora;
+    }
+
+    /**
+     * Get the value of data
+     */ 
+    public function getData(){
+        return $this->data;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId(){
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id){
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of data
+     *
+     * @return  self
+     */ 
+    public function setData($data){
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of hora
+     *
+     * @return  self
+     */ 
+    public function setHora($hora){
+        $this->hora = $hora;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of numeroPersones
+     *
+     * @return  self
+     */ 
+    public function setNumeroPersones($numeroPersones){
+        $this->numeroPersones = $numeroPersones;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of id_usuari
+     *
+     * @return  self
+     */ 
+    public function setId_usuari($id_usuari){
+        $this->id_usuari = $id_usuari;
+
+        return $this;
+    }
+}
--- model/Usuari/Usuari.php
+++ model/Usuari/Usuari.php
@@ -1,42 +1,65 @@
 <?php
 class Usuari{
-    private $id;
-    private $nom;
-    private $cognoms;
-    private $correu;
-    private $telefon;
-    private $rol;
-
+    private int $id;
+    private string $nom;
+    private string $cognoms;
+    private string $correu;
+    private string $telefon;
+    private string $rol;
     
+    public function __construct(int $id, string $nom, string $cognoms, string $correu, string $telefon, string $rol)
+        {
+            $this-> id = $id;
+            $this-> nom = $nom;
+            $this-> cognoms = $cognoms;
+            $this-> correu = $correu;
+            $this-> telefon = $telefon;
+            $this-> rol = $rol;
+        }
 
     /**
-     * Get the value of id, nom, cognom. correu. telefon i rol
+     * Get the value of id
      */ 
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Get the value of nom
+     */ 
     public function getNom()
     {
         return $this->nom;
     }
 
+    /**
+     * Get the value of cognoms
+     */ 
     public function getCognoms()
     {
         return $this->cognoms;
     }
 
+    /**
+     * Get the value of correu
+     */ 
     public function getCorreu()
     {
         return $this->correu;
     }
 
+    /**
+     * Get the value of telefon
+     */ 
     public function getTelefon()
     {
         return $this->telefon;
     }
 
+    /**
+     * Get the value of rol
+     */ 
     public function getRol()
     {
         return $this->rol;
@@ -44,61 +67,73 @@ class Usuari{
 
     /**
      * Set the value of id
+     *
      * @return  self
      */ 
     public function setId($id)
     {
         $this->id = $id;
+
         return $this;
     }
 
     /**
      * Set the value of nom
+     *
      * @return  self
      */ 
     public function setNom($nom)
     {
         $this->nom = $nom;
+
         return $this;
     }
 
     /**
      * Set the value of cognoms
+     *
      * @return  self
      */ 
     public function setCognoms($cognoms)
     {
         $this->cognoms = $cognoms;
+
         return $this;
     }
 
     /**
      * Set the value of correu
+     *
      * @return  self
      */ 
     public function setCorreu($correu)
     {
         $this->correu = $correu;
+
         return $this;
     }
 
     /**
      * Set the value of telefon
+     *
      * @return  self
      */ 
     public function setTelefon($telefon)
     {
         $this->telefon = $telefon;
+
         return $this;
     }
 
     /**
      * Set the value of rol
+     *
      * @return  self
      */ 
     public function setRol($rol)
     {
         $this->rol = $rol;
+
         return $this;
     }
 }
