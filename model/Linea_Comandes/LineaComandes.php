24a1eb579cffe133d7fca26fe1e7352fe578b407
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2025-11-13 16:01:07 +0100

    Creacio model Linea_Comandes 13/11/25
    Creacio de LineaConades i LineaComandesDAO en la carpeta Model/LineaComandes el dia 13/11/2025

---
+++ model/Linea_Comandes/LineaComandes.php
@@ -0,0 +1,97 @@
+<?php
+
+class LineaComandes{
+    private int $id;
+    private float $preuUnitat;
+    private int $idComanda;
+    private int $idProducte;
+    
+    public function __construct(int $id, float $preuUnitat, int $idComanda, int $idProducte){
+        $this-> id = $id;
+        $this-> preuUnitat = $preuUnitat;
+        $this-> idComanda = $idComanda;
+        $this-> idProducte = $idProducte;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of preuUnitat
+     */ 
+    public function getPreuUnitat()
+    {
+        return $this->preuUnitat;
+    }
+
+    /**
+     * Get the value of idComanda
+     */ 
+    public function getIdComanda()
+    {
+        return $this->idComanda;
+    }
+
+    /**
+     * Get the value of idProducte
+     */ 
+    public function getIdProducte()
+    {
+        return $this->idProducte;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of preuUnitat
+     *
+     * @return  self
+     */ 
+    public function setPreuUnitat($preuUnitat)
+    {
+        $this->preuUnitat = $preuUnitat;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of idComanda
+     *
+     * @return  self
+     */ 
+    public function setIdComanda($idComanda)
+    {
+        $this->idComanda = $idComanda;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of idProducte
+     *
+     * @return  self
+     */ 
+    public function setIdProducte($idProducte)
+    {
+        $this->idProducte = $idProducte;
+
+        return $this;
+    }
+}
+
+?>
