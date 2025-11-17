8549424e12dc25be102c92760655c92311df69a9
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2025-11-17 19:20:58 +0100

    Creacio del model Alergans 17/11/25

---
+++ model/Alergians/Alergians.php
@@ -0,0 +1,94 @@
+<?php
+class Alergians{
+    private int $id;
+    private string $nom;
+    private string $descripcio;
+    private string $imatge;
+    
+    public function __construct(string $nom, string $descripcio, string $imatge){
+        $this-> nom = $nom;
+        $this-> descripcio = $descripcio;
+        $this-> imatge = $imatge;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Get the value of descripcio
+     */ 
+    public function getDescripcio()
+    {
+        return $this->descripcio;
+    }
+
+    /**
+     * Get the value of imatge
+     */ 
+    public function getImatge()
+    {
+        return $this->imatge;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of descripcio
+     *
+     * @return  self
+     */ 
+    public function setDescripcio($descripcio)
+    {
+        $this->descripcio = $descripcio;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of imatge
+     *
+     * @return  self
+     */ 
+    public function setImatge($imatge)
+    {
+        $this->imatge = $imatge;
+
+        return $this;
+    }
+}
+?>
---
+++ model/Alergians/AlergiansDAO.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once 'model/Alergians/Alergians.php';
+include_once 'database/Database.php';
+
+class alergansDAO{
+    public static function getAlerganByID($id){
+        $con = DataBase::connect();
+        $stmt = $con->prepare("SELECT * FROM alergans where id = ?");
+        //si tenim mes camps podem fer aixo $stmt-> bind_param('iis',$id, $int2, $string);
+        $stmt-> bind_param('i',$id);
+        $stmt->execute();
+        $results = $stmt->get_result();
+        
+        $alergan = $results->fetch_object('Alergans');
+        $con->close();
+        
+        return $alergan;
+    }
+    public static function getAlergans(){
+        $con = DataBase::connect();
+        $stmt = $con->prepare("SELECT * FROM alergans");
+        $stmt->execute();
+
+        $results = $stmt->get_result();
+        
+        $listaAlergans = [];
+
+        while($alergan = $results->fetch_object('Alergans')){
+            $listaAlergans[]=$alergan;
+        }
+        
+        $con->close();
+        
+        return $listaAlergans;
+    }
+}
+
+?>
