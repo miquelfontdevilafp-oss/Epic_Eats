f1116b9c25dccf73efecbc7a7989a96d19ca2366
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2025-11-21 19:25:53 +0100

    Changes of footer problems with background color

--- view/footer.php
+++ view/footer.php
@@ -8,6 +8,44 @@
     <link rel="stylesheet" href="general.css">
 </head>
 <body>
-    
+    <footer class="container-fluid">
+        <div class="row d-flex justify-content-between">
+            <div class="col">
+                <a href=""><img src="" alt="Img Store"></a>
+            </div>
+            <div class="col d-flex justify-content-end">
+                <a href="">
+                    <img src="" alt="Img facebock">
+                </a>
+                <a href="">
+                    <img src="" alt="Img X">
+                </a>
+                <a href="">
+                    <img src="" alt="Img youtube">
+                </a>
+            </div>
+        </div>
+        <hr>
+        <div class="row d-flex justify-content-between text-center">
+            <a href="" class="col">Inici</a>
+            <a href="" class="col">Restaurant</a>
+            <a href="" class="col">La nostre carta</a>
+            <a href="" class="col">Reserva</a>
+            <a href="" class="col">Contacte</a>
+            <a href="" class="col">Avis legal</a>
+            <a href="" class="col">Configuracio de cookies</a>
+            <a href="" class="col">Politica de reembols</a>
+            <a href="" class="col">Termes d’us</a>
+        </div>
+        <hr>
+        <div class="row d-flex justify-content-between">
+            <div class="col-9">
+                <p>© 2025, Epic Eats, Inc. Todos los derechos reservados. Epic, Epic Games, el logotipo de Epic Games, Fortnite, el logotipo de Fortnite, Unreal, Unreal Engine, el logotipo de Unreal Engine, Unreal Tournament y el logotipo de Unreal Tournament son marcas comerciales o marcas registradas de Epic Games, Inc. tanto en Estados Unidos de América como en el resto del mundo. Otras marcas o nombres de productos son marcas comerciales de sus respectivos propietarios. Nuestros sitios web pueden incluir enlaces a otros sitios y recursos ofrecidos por terceros. Estos enlaces solo se ofrecen para tu mayor comodidad. Epic Games no tiene control sobre el contenido de esos sitios y recursos, y no acepta ninguna responsabilidad por ellos ni por cualquier pérdida o daño que pueda ocasionar el uso que haces de ellos.</p>
+            </div>
+            <div class="col d-flex justify-content-end">
+                <button class="d-flex">Torna a l'inici <img src="" alt="Img fletxa amun"></button>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
