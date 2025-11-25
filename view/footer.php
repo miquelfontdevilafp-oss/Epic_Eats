8692111c6f99c2ddc97aa7ee201d20357a0ccde6
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2025-11-25 19:09:24 +0100

    Actualitzacio Footer i Header

--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="general.css">
 </head>
-<body>
+<body> -->
     <footer class="container-fluid">
         <div class="row d-flex justify-content-between">
             <div class="col">
@@ -46,11 +46,11 @@
             </div>
             <div class="col d-flex justify-content-end">
                 <button class="d-flex">Torna a l'inici 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" class="svg24">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
             </div>
         </div>
     </footer>
-</body>
-</html>
+<!-- </body>
+</html> -->
--- view/header.php
+++ view/header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,41 +7,43 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="general.css">
 </head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
+<body> -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
 
-            <!-- Logo Epic Eats + desplegable amb php -->
-            <a class="navbar-brand" href="#">
-                <img src="/IMG/Logo_EpicEats_oscuro.png" height="32" alt="EpicEats">
-            </a>
+                <!-- Logo Epic Eats + desplegable amb php -->
+                <a class="navbar-brand" href="#">
+                    <img src="/IMG/Logo_EpicEats_oscuro.png" height="32" alt="EpicEats">
+                </a>
 
-            <!-- Menu de pagines -->
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="#">La Nostre Carta</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Restaurant</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Reserva</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Contacta</a></li>
-            </ul>
+                <!-- Menu de pagines -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#">La Nostre Carta</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Restaurant</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Reserva</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Contacta</a></li>
+                </ul>
 
-            <!-- Login/register + carro -->
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="navbar-brand d-flex" href="#">
-                        <p>Carro</p>
-                        <img src="/IMG/carro.png" alt="carro" height="32"
-                            class="d-inline-block align-text-top">
-                    </a>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="#">Link 1</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Link 2</a></li>
-            </ul>
+                <!-- Login/register + carro -->
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="navbar-brand d-flex" href="#">
+                            <p>Carro</p>
+                            <img src="/IMG/carro.png" alt="carro" height="32"
+                                class="d-inline-block align-text-top">
+                        </a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#">Link 1</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Link 2</a></li>
+                </ul>
 
-        </div>
-    </nav>
+            </div>
+        </nav>
+    </header>
 
-</body>
-</html>
+<!-- </body>
+</html> -->
 
 
 
