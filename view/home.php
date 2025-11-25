c9f33e1c43d7acbad99c38d1bd6f4001e6eade23
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2025-11-25 19:09:34 +0100

    Creacio Home

---
+++ view/home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="general.css">
+</head>
+<body>
+    <?php
+        include 'header.php';
+    ?>
+    <article>
+        <section id="sector1" class="row">
+            <div class="col">
+                <!-- background img -->
+                 <h2>Producte 1</h2>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quod animi perspiciatis, illum sint inventore, excepturi, molestiae vitae ipsa blanditiis mollitia corporis accusamus numquam cumque dolorem nemo beatae voluptas possimus.</p>
+                 <button>Demana ja</button>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <img src="" alt="img-producte" class="col">
+                    <h4 class="col">Producte 2</h4>
+                </div>
+                <div class="row">
+                    <img src="" alt="img-producte" class="col">
+                    <h4 class="col">Producte 3</h4>
+                </div>
+                <div class="row">
+                    <img src="" alt="img-producte" class="col">
+                    <h4 class="col">Producte 4</h4>
+                </div>
+                <div class="row">
+                    <img src="" alt="img-producte" class="col">
+                    <h4 class="col">Producte 5</h4>
+                </div>
+                <div class="row">
+                    <img src="" alt="img-producte" class="col">
+                    <h4 class="col">Producte 6</h4>
+                </div>
+                <div class="row">
+                    <img src="" alt="img-producte" class="col">
+                    <h4 class="col">Producte 7</h4>
+                </div>
+            </div>
+        </section>
+        <section>
+
+        </section>
+    </article>
+    <?php
+        include 'footer.php';
+    ?>
+</body>
+</html>
