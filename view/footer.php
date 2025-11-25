05fbe557032c4da5d6d17b2479c478119c96b2c2
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2025-11-25 17:55:46 +0100

    actualitzacions footer

--- view/footer.php
+++ view/footer.php
@@ -11,17 +11,19 @@
     <footer class="container-fluid">
         <div class="row d-flex justify-content-between">
             <div class="col">
-                <a href=""><img src="" alt="Img Store"></a>
+                <a href="">
+                    <img src="../IMG/Logo Epic Games Store.png" alt="Img Logo Store">
+                </a>
             </div>
             <div class="col d-flex justify-content-end">
                 <a href="">
-                    <img src="" alt="Img facebock">
+                    <img class="iconoRedSocial" src="../IMG/Logo Facebock.png" alt="Img facebock">
                 </a>
                 <a href="">
-                    <img src="" alt="Img X">
+                    <img class="iconoRedSocial" src="../IMG/Logo X.png" alt="Img X">
                 </a>
                 <a href="">
-                    <img src="" alt="Img youtube">
+                    <img class="iconoRedSocial" src="../IMG/Logo Youtube.png" alt="Img youtube">
                 </a>
             </div>
         </div>
@@ -43,7 +45,10 @@
                 <p>© 2025, Epic Eats, Inc. Todos los derechos reservados. Epic, Epic Games, el logotipo de Epic Games, Fortnite, el logotipo de Fortnite, Unreal, Unreal Engine, el logotipo de Unreal Engine, Unreal Tournament y el logotipo de Unreal Tournament son marcas comerciales o marcas registradas de Epic Games, Inc. tanto en Estados Unidos de América como en el resto del mundo. Otras marcas o nombres de productos son marcas comerciales de sus respectivos propietarios. Nuestros sitios web pueden incluir enlaces a otros sitios y recursos ofrecidos por terceros. Estos enlaces solo se ofrecen para tu mayor comodidad. Epic Games no tiene control sobre el contenido de esos sitios y recursos, y no acepta ninguna responsabilidad por ellos ni por cualquier pérdida o daño que pueda ocasionar el uso que haces de ellos.</p>
             </div>
             <div class="col d-flex justify-content-end">
-                <button class="d-flex">Torna a l'inici <img src="" alt="Img fletxa amun"></button>
+                <button class="d-flex">Torna a l'inici 
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
             </div>
         </div>
     </footer>
