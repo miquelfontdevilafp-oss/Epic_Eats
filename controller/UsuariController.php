f7681ef419bcbda08851bee53198b4279008d5e6
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2025-12-02 18:33:24 +0100

    Update UsuariController.php

--- controller/UsuariController.php
+++ controller/UsuariController.php
@@ -2,13 +2,13 @@
 include_once 'model\Usuari\UsuariDAO.php';
 class UsuariController{
     public function show(){
-        $view = 'view/usuari/show.php';
+        $view = 'view/Usuari/show.php';
         $idusuari=$_GET['idusuari'];
         $usuari = UsuariDAO::getUsuariByID($idusuari);
         include_once 'view/main.php';
     }
     public function index(){
-        $view = 'view/usuari/index.php';
+        $view = 'view/Usuari/index.php';
         $usuaris = UsuariDAO::getUsuaris();
         include_once 'view/main.php';
     }
